93d2fa4d4f972e3300422ef9254bce7a7bb6c031
Rodrigo Llull Torres <rllullt@gmail.com>
2025-08-18 23:19:32 -0400

    Add loader component

---
+++ product-explorer/src/app/components/loader/loader.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-loader',
+  imports: [CommonModule],
+  templateUrl: './loader.html',
+  styleUrl: './loader.scss'
+})
+export class Loader {
+
+}
