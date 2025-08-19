ae16e3b019291a77c1bab04c4c36b800f942667e
Rodrigo Llull Torres <rllullt@gmail.com>
2025-08-18 23:17:41 -0400

    Routes and item list component

--- product-explorer/src/app/app.routes.ts
+++ product-explorer/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ItemList } from './components/item-list/item-list';
+// import { ItemDetailComponent } from './components/item-detail.component';
+import { LandingPage } from './components/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: LandingPage
+    },
+    {
+        path: 'items',
+        component: ItemList
+    },
+    // {
+    //     path: 'items/:id',
+    //     component: ItemDetailComponent
+    // },
+];
---
+++ product-explorer/src/app/components/item-list/item-list.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+type inventoryItem = {
+  id: number;
+  name: string;
+  types: string;
+  abilities: string;
+  weight: number;
+  height: number;
+  url: string;
+};
+
+@Component({
+  selector: 'app-item-list',
+  imports: [MatListModule, MatIconModule],
+  templateUrl: './item-list.html',
+  styleUrl: './item-list.scss'
+})
+export class ItemList {
+  items: inventoryItem[] = [
+    {
+      id: 1,
+      name: 'Bulbasaur',
+      types: 'grass,poison',
+      abilities: 'overgrow,chlorophyll',
+      weight: 69,
+      height: 7,
+      url: 'https://pokeapi.co/api/v2/pokemon/1'
+    },
+    {
+      id: 2,
+      name: 'Ivysaur',
+      types: 'grass,poison',
+      abilities: 'overgrow,chlorophyll',
+      weight: 130,
+      height: 10,
+      url: 'https://pokeapi.co/api/v2/pokemon/2'
+    },
+    {
+      id: 3,
+      name: 'Venusaur',
+      types: 'grass,poison',
+      abilities: 'overgrow,chlorophyll',
+      weight: 1000,
+      height: 20,
+      url: 'https://pokeapi.co/api/v2/pokemon/3'
+    }
+  ];
+  // items: inventoryItem[] = [];
+}
